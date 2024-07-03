dfe73cebea90d7630faed5bba90979cfcef0fa7f
Gideon Blessing <blessingmacaulay@gmail.com>
2024-07-03 23:56:27 +0100

    latest changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-const currentTime = document.getElementById("year");
+/* const currentTime = document.getElementById("year");
 const thisYear = new Date().getFullYear();
 year.setAttribute("datetime", thisYear);
 year.textContent = thisYear;
+ */
+function displayDate() {
+	try {
+		const dateElement = document.getElementById("date");
+		const currentDate = new Date();
+		dateElement.innerHTML = currentDate.toUTCString();
+	} catch (error) {
+		console.error("An error occurred while displaying the date:", error);
+	}
+}
+
+window.onload = displayDate;
